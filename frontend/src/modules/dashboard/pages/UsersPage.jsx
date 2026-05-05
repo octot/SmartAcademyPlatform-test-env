b3fae98184b6b9fc3c1c99f7ab51df6d4dd75f25
AJAYKUMAR <aajaykumarr32@gmail.com>
2026-05-05 20:26:07 +0530

    feat : Frontend Added profileguard which checks for profile completion Changed approch from taking data from jwt to /me api

---
+++ frontend/src/modules/dashboard/pages/UsersPage.jsx
@@ -0,0 +1,12 @@
+export default function StudentsPage() {
+    return (
+        <div>
+            <h1>Users Page</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, voluptatibus magnam, distinctio officia incidunt iure sunt asperiores sit mollitia vitae nobis deleniti. Placeat eius consectetur eos veniam modi quod, rem quisquam natus, pariatur eaque veritatis inventore, expedita sequi fugit laudantium porro consequatur dignissimos accusamus perferendis corporis. Nam provident est culpa.
+            </p>
+        </div>
+
+    )
+
+}
