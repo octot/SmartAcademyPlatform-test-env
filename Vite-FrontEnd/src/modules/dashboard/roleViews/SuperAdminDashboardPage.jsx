46159259cacca18456ab54f9fdb7a05e1680c830
AJAYKUMAR <aajaykumarr32@gmail.com>
2026-06-16 21:09:22 +0530

    Feature : Frontend :Admin layout and sidebar Backend: Add extra comments for aboutUser method Added a flow for the admin For jwtauthentication  refactored the code split into modular methods better readability

---
+++ Vite-FrontEnd/src/modules/dashboard/roleViews/SuperAdminDashboardPage.jsx
@@ -0,0 +1,28 @@
+
+import "../styles/SuperAdminDashboardPage.css";
+
+
+
+export default function SuperAdminDashboardPage() {
+
+    return (
+
+        <div className="dashboard-container">
+
+            <div className="dashboard-header">
+
+                <h1>
+                    Welcome Back, Super Admin
+                </h1>
+
+                <p>
+                    Manage platform administration
+                    and review onboarding requests.
+                </p>
+
+            </div>
+
+        </div>
+
+    );
+}
